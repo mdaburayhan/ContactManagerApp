2d5f1662f3b00b15ca6f75f89e658c395f5528b2
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-23 16:55:43 +0600

    Add asynchronous add and delete operations in Repository using Executor
    - Updated Repository to perform addContact and deleteContact on a background thread
    - Used ExecutorService to prevent blocking the main (UI) thread during database writes
    - Prepared groundwork for asynchronous getAllContacts implementation
    - Improved app responsiveness and thread safety in database write operations

--- app/src/main/java/com/arsoft/contactmanagerapp/Repository.java
+++ app/src/main/java/com/arsoft/contactmanagerapp/Repository.java
@@ -1,7 +1,13 @@
 package com.arsoft.contactmanagerapp;
 
 
+import android.os.Handler;
+import android.os.Looper;
+
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 // Repository class that abstracts access to multiple data sources.
 // In this case, it manages data operations and provides a clean API for accessing Contact data.
@@ -12,21 +18,46 @@ public class Repository {
     // - ROOM Database
 
     private final ContactDAO contactDAO;
+    ExecutorService executor;
+    Handler handler;
 
     public Repository(ContactDAO contactDAO) {
         this.contactDAO = contactDAO;
+
+        // Creates a single-threaded executor to run tasks asynchronously on a background thread.
+        // This helps prevent blocking the main (UI) thread during database operations.
+        executor = Executors.newSingleThreadExecutor();
+
+        // Creates a Handler associated with the main (UI) thread's Looper.
+        // Used to post tasks that need to run on the main thread, such as updating the UI.
+        handler = new Handler(Looper.getMainLooper());
+
     }
 
     // Method in DAO being executed from Repository
     public void addContact(Contacts contacts){
-        contactDAO.insert(contacts);
+
+        // Runnable: Executing Task on Separate Thread
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                contactDAO.insert(contacts);
+            }
+        });
     }
     public void deleteContact(Contacts contacts){
-        contactDAO.delete(contacts);
-    }
 
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                contactDAO.delete(contacts);
+            }
+        });
+    }
     public List<Contacts> getAllContacts(){
         return contactDAO.getAllContacts();
     }
+
+
 
 }
