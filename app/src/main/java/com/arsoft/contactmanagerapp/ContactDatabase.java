31fe077f2772e142f37126282e21429705a13f03
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-23 16:19:52 +0600

    Implement Singleton pattern for ContactDatabase using synchronized getInstance() method to ensure single Room DB instance

--- app/src/main/java/com/arsoft/contactmanagerapp/ContactDatabase.java
+++ app/src/main/java/com/arsoft/contactmanagerapp/ContactDatabase.java
@@ -1,6 +1,9 @@
 package com.arsoft.contactmanagerapp;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 // The @Database annotation marks this class as a Room database.
@@ -17,5 +20,27 @@ public abstract class ContactDatabase extends RoomDatabase {
 
     // Connecting the database to the ContactDAO using this getContactDAO()
     public abstract ContactDAO getContactDAO();
+
+    /// Holds a "single instance" of the ContactDatabase to implement the Singleton pattern.
+    /// This ensures only one database instance exists throughout the app lifecycle.
+    private static ContactDatabase dbInstance;
+
+
+    // Returns the single instance of ContactDatabase.
+    // - Uses the Singleton pattern to avoid multiple instances.
+    // - 'synchronized' ensures thread safety: only one thread can access this method at a time.
+    // - This prevents race conditions when multiple threads try to create the database at once.
+    public static synchronized ContactDatabase getInstance(Context context){
+
+        if (dbInstance == null){
+            dbInstance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    ContactDatabase.class,
+                    "contacts_db"
+            ).fallbackToDestructiveMigration()
+                    .build();
+        }
+        return dbInstance;
+    }
 
 }
